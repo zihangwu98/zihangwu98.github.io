24920de3ba6266505085bec9491a6d9ed182a248
zihangwu98 <wuzihang98@gmail.com>
2026-09-01 17:47:39 +0200

    Configure GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
